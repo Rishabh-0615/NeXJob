a2dbcaa94d4a8819a1f832787143b4e62227f926
Pratik Chavan <pratikpchavan75@gmail.com>
2025-03-22 23:34:43 +0530

    controllers

--- backend/routes/recruiterRoutes.js
+++ backend/routes/recruiterRoutes.js
@@ -1,6 +1,13 @@
 import express from 'express'
+import { loginRecruiter, registerRecruiterWithOtp, verifyOtpAndRegisterRecruiter } from '../controllers/recruiterControllers.js';
 
 const router = express.Router();
+
+router.post('/registerRecruiter',registerRecruiterWithOtp);
+router.post('/verifyRecruiter/:token',verifyOtpAndRegisterRecruiter);
+router.post('/loginRecruiter',loginRecruiter);
+
+
 
 
 export default router;
